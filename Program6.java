dbdb45595ca29fd68074ed72959a527f5371a199
Ramya-03082003 <ramya.n2021eceb@sece.ac.in>
2025-01-25 09:39:33 +0530

    first commit

---
+++ Program6.java
@@ -0,0 +1,30 @@
+class Student {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+public class Program6 {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("John");
+        student.setAge(20);
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student Age: " + student.getAge());
+    }
+}
